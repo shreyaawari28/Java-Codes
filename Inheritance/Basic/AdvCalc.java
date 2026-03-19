21479d0889b5b4da2b719ea86dce768120855752
Shreya Awari <shreyaawari31@gmail.com>
2026-03-19 20:37:40 +0530

    Inheritance

---
+++ Inheritance/Basic/AdvCalc.java
@@ -0,0 +1,10 @@
+public class AdvCalc extends Calc {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int sub(int a,int b){
+        return a-b;
+    }
+}
---
+++ Inheritance/Basic/Calc.java
@@ -0,0 +1,11 @@
+public class Calc {
+    public int mul (int a,int b){
+        return a * b;
+    }
+
+    public int div (int a , int b){
+        return a / b;
+    }
+}
+
+
